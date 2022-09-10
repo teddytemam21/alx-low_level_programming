d0fc470e590dcd3abffb1fbfd8bda056ceb70e86
teddytemam21 <teddytemam21@gmail.com>
2022-09-10 05:25:28 -0700

    enter point

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,30 +1,27 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
- * main -  last digit greater than five */
+ * main -  Entry point
+ *
+ * Return:always 0 (Success)
+ */
 
 /* betty style doc for function main goes there */
 int main(void)
 {
  int n;
- int 1;
+ int L;
+ char str[] = "Last digit of";
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-1 = n % 10;
-if (1 > 5)
-{
-       printf("last digit of %d is %d and is greater than 5\n", n, 1);
-}
-else if (1 == 0)
-{
-        printf("last digit of %d is %d and is 0\n", n, 1);
-}
-else
-{
-	printf("last digit of %d is %d and is less than 6 and not 0\n", n, 1);
-}	
+L = n % 10;
+if (L > 5)
+       printf("%s  %d is %d and is greater than 5\n", str, n, L);
+else if (L == 0)
+        printf("%s %d is %d and is 0\n", str, n, 1);
+else if (L < 6)
+	printf("%s %d is %d and is less than 6 and not 0\n",str, n, L);	
 return (0);
 }
