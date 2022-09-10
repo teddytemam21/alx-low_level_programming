7bd832d85125b65a24198aa7d421820cc1049a04
teddytemam21 <teddytemam21@gmail.com>
2022-09-10 08:03:03 -0700

    alpha

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,9 +11,9 @@
 int main(void)
 {
 	char low;
-	for (low = 'a' ; low <= 'z' ; low++)
+	for (low = 'a' ; low <= 'z' ; low++);
 	putchar(low);
-	for (low = 'A' ; low <= 'Z' ; low++)
+	for (low = 'A' ; low <= 'Z' ; low++);
 	putchar(low);
 	putchar('\n');
 
