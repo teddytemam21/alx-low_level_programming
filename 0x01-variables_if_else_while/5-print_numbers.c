12b1d3dd9ad214507b2c3cab00cb8d325ccade03
teddytemam21 <teddytemam21@gmail.com>
2022-09-12 00:54:04 -0700

    5 alpha

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,7 +5,7 @@
  *  *  *  * main - print if the number is postive, zero, or negative
  *   *   *   *
  *    *    *    * Description: using the main function
- *     *     *     * this program prints "programming is postive, zero, or negative"
+ * this program prints "programming is postive, zero, or negative"
  *      *      *      * Return: 0
  */
 int main(void)
