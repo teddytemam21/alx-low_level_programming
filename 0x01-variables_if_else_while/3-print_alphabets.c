1d3613356d8d7eca24a6f50dcebc129a6fa65a6f
teddytemam21 <teddytemam21@gmail.com>
2022-09-12 00:31:10 -0700

    alpha

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,12 +2,13 @@
 #include <time.h>
 #include <stdio.h>
 /**
- *  main - print if the number is postive, zero, or negative
+ * main - print if the number is postive, zero, or negative
  *
- *  Description: using the main function
- *  this program prints "programing is postive, zero, or negative"
- *Return : 0
+ * Description: using the main function
+ * this program prints "programming is postive, zero, or negative"
+ * Return: 0
  */
+
 int main(void)
 {
 	char low, e, q;
