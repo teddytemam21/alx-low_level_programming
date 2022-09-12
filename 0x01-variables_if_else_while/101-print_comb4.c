8cfa2c3b0ab19983426906190792a86f4fe71641
teddytemam21 <teddytemam21@gmail.com>
2022-09-12 06:58:05 -0700

    alpha11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,29 +10,29 @@
  */
 int main(void)
 {
-	int i, j, k;
+int i, j, k;
 
-	for (i = 0; i <= 9; i++)
-	{
-		for (j = 1; j <= 9; j++)
-		{
-			for (k = 2; k <= 9; k++)
-			{
-				if (j > i)
-					if (k > j)
-					{
-						putchar(i + '0');
-						putchar(j + '0');
-						putchar(k + '0');
-						if (i != 7)
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+for (i = 0; i <= 9; i++)
+{
+for (j = 1; j <= 9; j++)
+{
+for (k = 2; k <= 9; k++)
+{
+if (j > i)
+if (k > j)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
+if (i != 7)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
