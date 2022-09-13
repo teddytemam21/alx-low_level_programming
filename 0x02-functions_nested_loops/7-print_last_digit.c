9fe0b970990cf8c73ef0749b7854988f40430d78
teddytemam21 <teddytemam21@gmail.com>
2022-09-13 08:53:54 -0700

    nest 6-8

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,11 @@
  */
 int print_last_digit(int n)
 {
-int x;
+int last_digit = n % 10;
 
-if (n < 0)
-n = -n;
-x = n % 10;
-_putchar(x + '0');
+if (last_digit < 0)
+	last_digit *= -1;
+_putchar(last_digit + '0');
 
-return (x);
+return (last_digit);
 }
