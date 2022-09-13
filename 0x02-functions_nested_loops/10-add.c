c5032c31e8c11b3e8035cc7c8b5ec4b1608f5924
teddytemam21 <teddytemam21@gmail.com>
2022-09-13 09:19:04 -0700

    nest 6-8

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two intigers.
+ * @num1: the first intiger to be added
+ * @num2: thesecond intiger to be added
+ *
+ * Return: the result of the addition
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2)
+}
