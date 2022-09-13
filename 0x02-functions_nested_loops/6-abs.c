8406d5e26234cb8c3dbb2d7e50587a59b5e38c12
teddytemam21 <teddytemam21@gmail.com>
2022-09-13 08:17:46 -0700

    nest 6-8

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * * _abs -> prints absolute value of a number
+ * * @n: parameter
+ * * Return: absolute value of a number
+ */
+int print_sign(int n)
+{
+	if (n < 0)
+		n = -n;
+	return (n);
+}
