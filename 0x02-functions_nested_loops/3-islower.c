ef4ac53980b29c9e63619e7a36e9ae29594c3491
teddytemam21 <teddytemam21@gmail.com>
2022-09-13 07:54:40 -0700

    nest 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * * _islower -> checks if the character is lowercase
+ * * @c: a character argument
+ * * Return: returnes 1 and 0 depening on condition
+ * */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
