d1c2b5e6f18ee935c6d40fb3272dfcc2db997011
teddytemam21 <teddytemam21@gmail.com>
2022-09-13 09:46:35 -0700

    nest 6-8

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * * _abs -> prints absolute value of a number
- * * @n: parameter
- * * Return: absolute value of a number
+ * * _abs - functions that computes the absolute value of an intiger
+ * * @c: is the int that will use for the argument of the function
+ * * Return: 0
  */
 
 int _abs(int c)
