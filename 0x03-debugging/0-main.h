6da0150745de0dcb88124a52a63e0c9095f3ce6a
teddytemam21 <teddytemam21@gmail.com>
2022-09-14 05:46:34 -0700

    debug

---
+++ 0x03-debugging/0-main.h
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - test function that print if integer is postive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+
+i = 0;
+postive_or_negative(i);
+
+return (0);
+}
+
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void postive_or_negative(int i);
+int largest_number(int, int, int);
+int convert_day(int, int);
+
+#endif /* MAIN_H*/
